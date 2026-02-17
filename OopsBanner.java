8bf6b044a8e19d711fd69b9499e457892fe9dff1
mega1906 <mega.dioscuri@gmail.com>
2026-02-17 16:58:19 +0530

    oops banner using system.out.println

---
+++ OopsBanner.java
@@ -0,0 +1,21 @@
+/** 
+* OOPSBANNERAPP UC2 - OOPS BANNER Display Application
+*
+* This class demonstrates a simple Java application that displays the Object
+*Oriented Programming System OOPS acronym to the console.
+* @author Developer
+* @version 2.0
+**/
+class OopsBanner{
+	public static void main(String[] args){
+		System.out.println("   ***   " + " " + "   ***   " + " " + " ******   " + "  ***** ");
+		System.out.println(" **   **" + " " + "  **   **" + " " + "  **   **" + " " +  " **");
+		System.out.println("**     **" + " " + "**     **" + " " + " **    **" + " " + "**");
+		System.out.println("**     **" + " " + "**     **" + "  **   **" + " " + " " + " **");
+		System.out.println("**     **" + " " + "**     **" + " " + " ****** " + " " + "   ****");
+		System.out.println("**     **" + " " + "**     **" + " " + " **     " + " " + "      **");
+		System.out.println("**     **" + " " + "**     **" + " " + " **     " + " " + "       **");
+		System.out.println(" **   **" + " " + "  **   **" + " " + "  **     " + " " + "      **");
+		System.out.println("   ***   " + " " + "   ***   " + " " + " **     " + "  ***** ");
+	}
+}
