0e4552cdcada5f9f992c75aabddea4c8ae2b8f64
mega1906 <mega.dioscuri@gmail.com>
2026-02-17 14:58:32 +0530

    comments print oops

--- Message.java
+++ Message.java
@@ -1,5 +1,13 @@
+/** 
+* OOPSBANNERAPP UC1 - OOPS BANNER Display Application
+*
+* This class demonstrates a simple Java application that displays the Object
+*Oriented Programming System OOPS acronym to the console.
+* @author Developer
+* @version 1.0
+**/
 class Message{
 	public static void main(String[] args){
-		System.out.print("Hello");
+		System.out.print("OOPS");
 	}
 }
